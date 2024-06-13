2a61852c3d588a11519e3e80ae100584a1241caa
gameboo <alexandre.joannou@gmail.com>
2024-06-13 17:12:35 +0100

    Adding a serial-lite3 wrapper component

--- DE10Pro-cheri-bgas.v
+++ DE10Pro-cheri-bgas.v
@@ -43,10 +43,10 @@
 `define ENABLE_TEMP_I2C
 //`define ENABLE_GPIO
 //`define ENABLE_PCIE
-//`define ENABLE_QSFP28A
-//`define ENABLE_QSFP28B
-//`define ENABLE_QSFP28C
-//`define ENABLE_QSFP28D
+`define ENABLE_QSFP28A
+`define ENABLE_QSFP28B
+`define ENABLE_QSFP28C
+`define ENABLE_QSFP28D
 `define ENABLE_HPS
 
 module DE10Pro_cheri_bgas (
@@ -498,6 +498,30 @@ module DE10Pro_cheri_bgas (
   , .fan_controller_reset_source_reset_n           (fan_reset_n)
   `endif // ENABLE_TEMP_I2C
   `endif // ENABLE_FAN_I2C
+  `ifdef ENABLE_QSFP28A
+  //, .seriallite3_wrapper_coe_link_status_a_coe_link_status           (link_status_A)
+  , .seriallite3_wrapper_coe_qsfp28_rx_pins_x_a_coe_qsfp28_rx_pins_x (QSFP28A_RX_p)
+  , .seriallite3_wrapper_coe_qsfp28_tx_pins_a_coe_qsfp28_tx_pins     (QSFP28A_TX_p)
+  , .seriallite3_wrapper_csi_qsfp_refclk_a_clk                       (QSFP28A_REFCLK_p)
+  `endif // ENABLE_QSFP28A
+  `ifdef ENABLE_QSFP28B
+  //, .seriallite3_wrapper_coe_link_status_b_coe_link_status           (link_status_B)
+  , .seriallite3_wrapper_coe_qsfp28_rx_pins_x_b_coe_qsfp28_rx_pins_x (QSFP28B_RX_p)
+  , .seriallite3_wrapper_coe_qsfp28_tx_pins_b_coe_qsfp28_tx_pins     (QSFP28B_TX_p)
+  , .seriallite3_wrapper_csi_qsfp_refclk_b_clk                       (QSFP28B_REFCLK_p)
+  `endif // ENABLE_QSFP28B
+  `ifdef ENABLE_QSFP28C
+  //, .seriallite3_wrapper_coe_link_status_c_coe_link_status           (link_status_C)
+  , .seriallite3_wrapper_coe_qsfp28_rx_pins_x_c_coe_qsfp28_rx_pins_x (QSFP28C_RX_p)
+  , .seriallite3_wrapper_coe_qsfp28_tx_pins_c_coe_qsfp28_tx_pins     (QSFP28C_TX_p)
+  , .seriallite3_wrapper_csi_qsfp_refclk_c_clk                       (QSFP28C_REFCLK_p)
+  `endif // ENABLE_QSFP28C
+  `ifdef ENABLE_QSFP28D
+  //, .seriallite3_wrapper_coe_link_status_d_coe_link_status           (link_status_D)
+  , .seriallite3_wrapper_coe_qsfp28_rx_pins_x_d_coe_qsfp28_rx_pins_x (QSFP28D_RX_p)
+  , .seriallite3_wrapper_coe_qsfp28_tx_pins_d_coe_qsfp28_tx_pins     (QSFP28D_TX_p)
+  , .seriallite3_wrapper_csi_qsfp_refclk_d_clk                       (QSFP28D_REFCLK_p)
+  `endif // ENABLE_QSFP28D
   );
 
 endmodule
