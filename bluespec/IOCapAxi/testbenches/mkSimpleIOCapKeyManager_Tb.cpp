65e7c12ffa0dc1342a80ade9c12fde24a8a0e75e
Samuel Stark <sws35@cam.ac.uk>
2024-07-10 17:57:31 +0100

    More boilerplate for unit tests

--- bluespec/IOCapAxi/testbenches/mkSimpleIOCapKeyManager_Tb.cpp
+++ bluespec/IOCapAxi/testbenches/mkSimpleIOCapKeyManager_Tb.cpp
@@ -1,3 +1,40 @@
-int main(void) {
+#include <verilated.h>
+#include "VmkSimpleIOCapKeyManager_Tb.h"
+
+int main(int argc, char** argv) {
+    Verilated::commandArgs (argc, argv);    // remember args
     
+    {
+        // Make a design-under-test
+        VmkSimpleIOCapKeyManager_Tb dut{};
+
+        uint64_t main_time = 0;
+        // initial conditions in order to generate appropriate edges on
+        // reset
+        dut.RST_N = 1;
+        dut.CLK = 0;
+
+        while (!Verilated::gotFinish()) { // $finish executed
+            if (main_time == 2) {
+                dut.RST_N = 0;    // assert reset
+            }
+            else if (main_time == 7) {
+                dut.RST_N = 1;    // Deassert reset
+            }
+
+            // Toggle clock
+            if ((main_time % 10) == 5) {
+                dut.CLK = 1;
+            }
+            else if ((main_time % 10) == 0) {
+                dut.CLK = 0;
+            }
+
+            dut.eval();
+            main_time++;
+        }
+
+        dut.final();    // Done simulating
+        // end of DUT lifetime, gets destructed
+    }
 }
