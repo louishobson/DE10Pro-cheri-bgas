6c5470f02f5462a18d7db3b14dd8602e2b7e36e8
Samuel Stark <sws35@cam.ac.uk>
2024-11-22 15:51:53 +0000

    Create fastfsm 2024_11 from 2024_02 [WIP]

---
+++ bluespec/IOCapAxi/testbenches/mkCap2024_11_Decode_FastFSM_Tb.cpp
@@ -0,0 +1,22 @@
+#include <verilated.h>
+#include "VmkCap2024_11_Decode_FastFSM_Tb.h"
+
+#include "decode_tests_uvm.h"
+
+using TheDUT = VmkCap2024_11_Decode_FastFSM_Tb;
+
+int main(int argc, char** argv) {
+    std::vector<TestBase*> tests = {
+        new DecoderUVMishTest_11<TheDUT>(new ManyRandomValidCaps_11<TheDUT>(2000)),  
+        new DecoderUVMishTest_11<TheDUT>(new ManyLibRustRandomValidCaps_11<TheDUT>(2000)),  
+        new DecoderUVMishTest_11<TheDUT>(new ManyRandomBits<TheDUT>(2000)),  
+    };
+
+    for (auto edge_case = 0; edge_case < ccap2024_11_rand_edge_case_num(); edge_case++) {
+        tests.push_back(
+            new DecoderUVMishTest_11<TheDUT>(new ManyLibRustEdgeCaseCaps_11<TheDUT>(2000, edge_case))
+        );
+    }
+
+    return tb_main(tests, argc, argv);
+}
